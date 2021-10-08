ce066f0afa0f3720b0db1760c43d858b53353c26
Devil-Beichen <2394439184@qq.com>
2021-10-08 21:49:18 +0800

    init

---
+++ Source/EZGame_AutoChess/Public/EZGame_AutoChessModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EZGame_AutoChessModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EZGAME_AUTOCHESS_API AEZGame_AutoChessModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
